2496baedb9b11ea827a5dfb12503920ab5aed2bc
MinChangGH <799022552@qq.com>
2019-12-27 18:00:01 +0800

    进度条

--- src/view/audio/audio.vue
+++ src/view/audio/audio.vue
@@ -5,19 +5,28 @@
         <li :class="{'curline': index==actIndex}" v-for="(item,index) in lrcData">{{item.lrc}}</li>
       </ul>
     </div>
-    <div class="audWrap">
+    <div class="audWrap" @mouseup="mUp($event)" @mousemove="mMove($event)">
       <div class="play">
         <i v-if="!isplay" @click="play" class="el-icon-video-play"></i>
-        <i v-if="isplay"@click="pause" class="el-icon-video-pause"></i>
+        <i v-if="isplay" @click="pause" class="el-icon-video-pause"></i>
       </div>
-      <div class="line">
-        <div class="progress" :style="progress"></div>
+      <div ref="line" class="line">
+        <div class="progress" :style="progress">
+
+        </div>
+        <i @mousedown="mDown($event)" :style="circle" class="el-icon-s-help"></i>
       </div>
-      <audio ref="aud"  src="/static/mp3/lrc.mp3"></audio>
+      <audio ref="aud" src="/static/mp3/lrc.mp3"></audio>
     </div>
-
-
   </div>
+
+
+  <!--    <div>-->
+  <!--      <input  v-model="value" @change="rangeVal" type="range">-->
+  <!--    </div>-->
+
+
+  <!--  </div>-->
 </template>
 
 <script>
@@ -27,15 +36,27 @@
     name: 'audio',
     data() {
       return {
-        duration:null,
+        index: 1,
+        percent: null,
+        isdown: false,
+        lineW: 400,
+        beginLeft: 0,
+        endLeft: null,
+        startX: null,
+        endX: null,
+        value: '',
+        duration: null,
         progress: {
           width: '0%'
+        },
+        circle: {
+          left: '0%'
         },
         isplay: false,
         actIndex: 0,
         lrcData: null,
         styl: {
-          backgroundImage:'url(/static/img/1.jpg)'
+          backgroundImage: 'url(/static/img/1.jpg)'
         }
       }
     },
@@ -45,49 +66,109 @@
       this.getLrc()
     },
     mounted() {
+      this.lineW = this.$refs.line.offsetWidth
       let self = this
       this.animate()
-      this.$refs.aud.addEventListener("canplay", function(){//监听audio是否加载完毕，如果加载完毕，则读取audio播放时间
+      this.$refs.aud.addEventListener("canplay", function () {//监听audio是否加载完毕，如果加载完毕，则读取audio播放时间
         self.duration = (self.$refs.aud.duration).toFixed(3)
         let arr = self.duration.toString().split('.')
-        self.duration = arr[0]*1000 + parseInt(arr[1])
+        self.duration = arr[0] * 1000 + parseInt(arr[1])
       })
 
-      this.$refs.aud.addEventListener("timeupdate",function(){
+      this.$refs.aud.addEventListener("timeupdate", function () {
         var timeDisplay = (self.$refs.aud.currentTime).toFixed(3)
         self.progress = {
-          width: `${((self.$refs.aud.currentTime/self.duration)*100000).toFixed(2)}%`
-        }
-        console.log(self.$refs.aud.currentTime/self.duration*100000)
-        let arr = timeDisplay.toString().split('.')
-        let total = arr[0]*1000 + parseInt(arr[1])
-        for (let i= 0;i<self.lrcData.length;i++) {
-          if (self.lrcData[i].time<total&& self.lrcData[i+1].time>total) {
-            self.actIndex = i
+          width: `${((self.$refs.aud.currentTime / self.duration) * 100000).toFixed(2)}%`
 
-            self.$refs.list.style.top= `-${i*30}px`
+        }
+        self.circle = {
+          left: `${((self.$refs.aud.currentTime / self.duration) * 100000).toFixed(2)}%`
+        }
+        let arr = timeDisplay.toString().split('.')
+        let total = arr[0] * 1000 + parseInt(arr[1])
+        for (let i = 0; i < self.lrcData.length; i++) {
+          if (self.lrcData[i].time < total && self.lrcData[i + 1].time > total) {
+            self.actIndex = i
+            self.$refs.list.style.top = `-${i * 30}px`
             break;
           }
         }
-      },false)
+      }, false)
 
-      this.$refs.aud.addEventListener('playing', function () {
-        // self.loopLrc()
-      }, false);
+      // this.$refs.aud.addEventListener('playing', function () {
+      //   // self.loopLrc()
+      // }, false);
     },
 
     methods: {
-      animate () {
+      seltime () {
+        let a = new Date(this.value1).getTime()
+        console.log(a)
+      },
+      mOver(e) {
+        console.log(e.target.style.left)
+      },
+      mDown(e) {
+        let self = this
+        this.isdown = true
+        this.$refs.aud.pause()
+        // window.addEventListener('mousemove',self.mMove(e))
+        this.startX = e.x
+      },
+      mMove(e) {
+        if (!this.isdown) {
+          return
+        }
+        let dif = e.x - this.startX
+        let persent = this.progress.width.toString().split('%')[0] / 100
+        let progressW = this.lineW * (persent / 100)
+        let positionNow = progressW + dif
+        if (this.index>=2) {
+          console.log(this.progress.width)
+          alert(positionNow)
+        }
+
+        if (positionNow < 0) {
+          positionNow = 0
+          this.progress.width = '0px'
+          this.circle.left = '0px'
+          return
+        }
+        if (positionNow > this.lineW) {
+          positionNow = 400
+          this.progress.width = '100%'
+          this.circle.left = '100%'
+          return;
+        }
+        // console.log(positionNow)
+        this.percent = positionNow / this.lineW
+        this.progress.width = `${((positionNow / this.lineW) * 100).toFixed(2)}%`
+        // console.log(this.progress)
+        this.circle.left = `${((positionNow / this.lineW) * 100).toFixed(2)}%`
+      },
+      mUp(e) {
+        let self = this
+        this.index++
+        this.isdown = false
+        this.$refs.aud.currentTime = Math.floor(this.$refs.aud.duration * this.percent)
+        this.$refs.aud.play()
+        this.isplay = true
+
+      },
+      rangeVal(val) {
+        console.log(this.value)
+      },
+      animate() {
         let i = 0
-        setInterval(()=>{
+        setInterval(() => {
           i++
-          if(i>=7) {
-            i=1
+          if (i >= 7) {
+            i = 1
           }
           this.styl = {
-            backgroundImage:`url(/static/img/${i}.jpg)`
+            backgroundImage: `url(/static/img/${i}.jpg)`
           }
-        },5000)
+        }, 5000)
       },
       getLrc() {
         api.get({}, '/home/3').then((res) => {
@@ -95,11 +176,11 @@
 
         })
       },
-      play () {
+      play() {
         this.isplay = true
         this.$refs.aud.play()
       },
-      pause () {
+      pause() {
         this.isplay = false
         this.$refs.aud.pause()
       },
@@ -121,22 +202,25 @@
     font-weight: 700;
     font-size: 25px;
   }
+
   .wrap {
     position: relative;
     width: 600px;
     margin: 0 auto;
     height: 400px;
-    padding-top: 30px ;
+    padding-top: 30px;
     transition: .2s;
     box-sizing: border-box;
     background-repeat: no-repeat;
     background-size: cover;
+
     .listwrap {
       margin-top: 30px;
       width: 100%;
       height: 240px;
       overflow: hidden;
       position: relative;
+
       .list {
         margin-top: 80px;
         position: absolute;
@@ -148,12 +232,14 @@
         text-align: center;
         font-size: 20px;
         transition: .5s;
+
         li {
-          line-height:30px ;
+          line-height: 30px;
           height: 30px;
         }
       }
     }
+
     .audWrap {
       padding: 0 50px;
       box-sizing: border-box;
@@ -166,10 +252,12 @@
       align-items: center;
       color: #0BD6A6;
       font-size: 50px;
+
       .play {
         cursor: pointer;
         width: 50px;
       }
+
       .line {
         cursor: pointer;
         margin-left: 20px;
@@ -177,11 +265,25 @@
         height: 2px;
         background: #ffffff;
         box-sizing: border-box;
+        position: relative;
+
         .progress {
-          background:#0BD6A6 ;
+          background: #0BD6A6;
           height: 2px;
-          transition: 1s;
         }
+
+        i {
+          position: absolute;
+          left: -2px;
+          top: -4px;
+          font-size: 8px;
+          margin-left: -2px;
+        }
+
+        &:hover i {
+          display: block;
+        }
+
       }
     }
   }
